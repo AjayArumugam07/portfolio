c9ae3e4c88e4b2271d47a5699a052799200d5115
Ajay Arumugam <ajayarumugam07@gmail.com>
2023-04-21 21:27:46 -0600

    created section wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
-import ConstructionBanner from "./components/ConstructionBanner"
-
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from './components'
+import ConstructionBanner from './components/ConstructionBanner'
 
 function App() {
-
   return (
     <BrowserRouter>
-      
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-no-repeat bg-cover bg-center">
+      <div className='relative z-0'>
+        <div className='bg-hero-pattern bg-no-repeat bg-cover bg-center'>
           <div>
-          <ConstructionBanner />
-          <Navbar />
+            <ConstructionBanner />
+            <Navbar />
           </div>
-         
           <Hero />
+        </div>
+        <div className='bg-[#050816]'>
+          <Experience />
         </div>
 
         {/* <About />
-        <Experience />
+        
         <Tech />
         <Works />
         <Feedbacks />
